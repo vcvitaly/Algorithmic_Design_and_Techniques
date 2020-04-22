bd0b4d898b56acf1f4f5ce0b492b06b9457bf18d
Vitaly.Chura <vcvitaly@yahoo.com>
2020-04-22 11:50:46 +0300

    Refactor

--- src/main/java/io/github/vcvitaly/algo/design/warmup/LCM.java
+++ src/main/java/io/github/vcvitaly/algo/design/warmup/LCM.java
@@ -14,7 +14,7 @@ public class LCM {
         return gcdFast(b, a % b);
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
