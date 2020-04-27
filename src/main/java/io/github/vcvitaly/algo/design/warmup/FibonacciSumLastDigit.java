790e687c98241a1d7926950edeb3f643fc27bcbb
Vitaly.Chura <vcvitaly@yahoo.com>
2020-04-27 19:08:39 +0300

    Solve FibonacciSumLastDigit

---
+++ src/main/java/io/github/vcvitaly/algo/design/warmup/FibonacciSumLastDigit.java
@@ -0,0 +1,65 @@
+package io.github.vcvitaly.algo.design.warmup;
+
+import java.util.Scanner;
+
+public class FibonacciSumLastDigit {
+
+    private static final int PERIOD_OF_10 = 60;
+
+    static long getFibonacciSumNaive(long n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        long previous = 0,
+             current  = 1,
+             sum      = 1;
+
+        for (long i = 0; i < n - 1; ++i) {
+            long tmpPrevious = previous;
+            previous = current;
+            current = tmpPrevious + current;
+            sum += current;
+        }
+
+        return sum % 10;
+    }
+
+    static long getFibonacciSumFast(long n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        long nPlus2LastDigit = getLastDigitOfFibN(n + 2);
+        return nPlus2LastDigit == 0 ? 9 : nPlus2LastDigit - 1;
+    }
+
+    static long getLastDigitOfFibN(long n) {
+        return calcFib((int) (n % PERIOD_OF_10)) % 10;
+    }
+
+    private static long calcFib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        Long[] numbers = new Long[n+1];
+        numbers[0] = 0L;
+        numbers[1] = 1L;
+
+        for (int i = 2; i <= n; i++) {
+            numbers[i] = numbers[i-1] + numbers[i-2];
+        }
+
+        return numbers[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long s = getFibonacciSumFast(n);
+        System.out.println(s);
+    }
+}
+
+
