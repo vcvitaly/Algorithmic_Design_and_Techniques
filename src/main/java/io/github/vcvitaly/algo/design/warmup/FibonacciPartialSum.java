d5c2d42dab2f66c1f41e4fe395a97d825e060724
Vitaly.Chura <vcvitaly@yahoo.com>
2020-04-28 13:51:37 +0300

    Solve FibonacciPartialSum

---
+++ src/main/java/io/github/vcvitaly/algo/design/warmup/FibonacciPartialSum.java
@@ -0,0 +1,50 @@
+package io.github.vcvitaly.algo.design.warmup;
+
+import java.util.Scanner;
+
+public class FibonacciPartialSum {
+
+    private static final int PERIOD_OF_10 = 60;
+
+    static long getFibonacciPartialSumFast(long from, long to) {
+        if (to <= 1) {
+            return to;
+        }
+
+        long fromMinus1LastDigit = getLastDigitOfFibSumUpto(from - 1);
+        long toLastDigit = getLastDigitOfFibSumUpto(to);
+
+        return (toLastDigit - fromMinus1LastDigit + 10) % 10;
+    }
+
+    private static long getLastDigitOfFibSumUpto(long to) {
+        return ((getLastDigitOfFibN(to + 2) - 1) + 10) % 10;
+    }
+
+    static long getLastDigitOfFibN(long n) {
+        return calcFib((int) (n % PERIOD_OF_10)) % 10;
+    }
+
+    private static long calcFib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        Long[] numbers = new Long[n+1];
+        numbers[0] = 0L;
+        numbers[1] = 1L;
+
+        for (int i = 2; i <= n; i++) {
+            numbers[i] = numbers[i-1] + numbers[i-2];
+        }
+
+        return numbers[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long from = scanner.nextLong();
+        long to = scanner.nextLong();
+        System.out.println(getFibonacciPartialSumFast(from, to));
+    }
+}
---
+++ src/test/java/io/github/vcvitaly/algo/design/warmup/FibonacciPartialSumTest.java
@@ -0,0 +1,43 @@
+package io.github.vcvitaly.algo.design.warmup;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.rnorth.ducttape.timeouts.Timeouts;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FibonacciPartialSumTest {
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void findsLastDigitOfFibonacciSum(Param param) {
+        System.out.println(param);
+        assertThat(
+                Timeouts.getWithTimeout(1_500, TimeUnit.MILLISECONDS,
+                        () -> FibonacciPartialSum.getFibonacciPartialSumFast(param.m, param.n))
+
+        ).isEqualTo(param.lastDigitofSum);
+    }
+
+    static Stream<Param> params() {
+        return Stream.of(
+                Param.of(0, 3, 4),
+                Param.of(3, 7, 1),
+                Param.of(10, 10, 5),
+                Param.of(10, 200, 2),
+                Param.of(33_333, 10_000, 9)
+        );
+    }
+
+    @Data
+    @AllArgsConstructor(staticName = "of")
+    private static class Param {
+        private long m;
+        private long n;
+        private int lastDigitofSum;
+    }
+}
