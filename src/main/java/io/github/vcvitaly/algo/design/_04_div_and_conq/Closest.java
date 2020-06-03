7ff54040ba66d2cc489cd1dd7b680de98ff6aa30
Vitaly.Chura <vcvitaly@yahoo.com>
2020-06-03 10:04:39 +0300

    Draft Closest

---
+++ src/main/java/io/github/vcvitaly/algo/design/_04_div_and_conq/Closest.java
@@ -0,0 +1,67 @@
+package io.github.vcvitaly.algo.design._04_div_and_conq;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Closest {
+
+    static class Point implements Comparable<Point> {
+        long x, y;
+
+        public Point(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return o.y == y ? Long.signum(x - o.x) : Long.signum(y - o.y);
+        }
+    }
+
+    static double minimalDistance(int[] x, int y[]) {
+        double ans = Double.POSITIVE_INFINITY;
+        //write your code here
+        return ans;
+    }
+
+    public static void main(String[] args) throws Exception {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(System.out);
+        int n = nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = nextInt();
+            y[i] = nextInt();
+        }
+        System.out.println(minimalDistance(x, y));
+        writer.close();
+    }
+
+    static BufferedReader reader;
+    static PrintWriter writer;
+    static StringTokenizer tok = new StringTokenizer("");
+
+
+    static String next() {
+        while (!tok.hasMoreTokens()) {
+            String w = null;
+            try {
+                w = reader.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (w == null)
+                return null;
+            tok = new StringTokenizer(w);
+        }
+        return tok.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
