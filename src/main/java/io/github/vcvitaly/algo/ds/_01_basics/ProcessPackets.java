591b19c852f63942f77b6ce3a4035643e73b43a9
Vitaly.Chura <vcvitaly@yahoo.com>
2020-06-09 21:00:43 +0300

    Solve ProcessPackets

--- src/main/java/io/github/vcvitaly/algo/ds/_01_basics/ProcessPackets.java
+++ src/main/java/io/github/vcvitaly/algo/ds/_01_basics/ProcessPackets.java
@@ -1,7 +1,9 @@
 package io.github.vcvitaly.algo.ds._01_basics;
 
-import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 class Request {
@@ -11,6 +13,28 @@ class Request {
     public Request(int arrivalTime, int processTime) {
         this.arrivalTime = arrivalTime;
         this.processTime = processTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "arrivalTime=" + arrivalTime +
+                ", processTime=" + processTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Request request = (Request) o;
+        return arrivalTime == request.arrivalTime &&
+                processTime == request.processTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(arrivalTime, processTime);
     }
 }
 
@@ -22,27 +46,66 @@ class Response {
         this.dropped = dropped;
         this.startTime = startTime;
     }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "dropped=" + dropped +
+                ", startTime=" + startTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Response response = (Response) o;
+        return dropped == response.dropped &&
+                startTime == response.startTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dropped, startTime);
+    }
 }
 
 class Buffer {
+    static final int PACKET_WAS_DROPPED_START_TIME = -1;
     private int size;
-    private List<Integer> finishTime;
+    private Deque<Integer> finishTimeQ;
 
     public Buffer(int size) {
         this.size = size;
-        this.finishTime = new ArrayList<>();
+        this.finishTimeQ = new LinkedList<>();
     }
 
     public Response process(Request request) {
-        // write your code here
-        return new Response(false, -1);
+//        System.out.println("Processing " + request);
+
+        while (!finishTimeQ.isEmpty() && finishTimeQ.peek() <= request.arrivalTime) {
+            finishTimeQ.poll();
+        }
+
+        if (finishTimeQ.size() == size) {
+            Response response = new Response(true, PACKET_WAS_DROPPED_START_TIME);
+//            System.out.println("Buffer is full, dropping the request: " + response);
+            return response;
+        }
+
+        int startTime = finishTimeQ.isEmpty() ? request.arrivalTime : finishTimeQ.peekLast();
+        finishTimeQ.add(startTime + request.processTime);
+
+        Response response = new Response(false, startTime);
+//        System.out.println("Processing the request: " + request);
+        return response;
     }
 }
 
 class ProcessPackets {
     static List<Request> readQueries(Scanner scanner) {
         int requestsCount = scanner.nextInt();
-        List<Request> requests = new ArrayList<>();
+        List<Request> requests = new LinkedList<>();
         for (int i = 0; i < requestsCount; ++i) {
             int arrivalTime = scanner.nextInt();
             int processTime = scanner.nextInt();
@@ -52,7 +115,7 @@ class ProcessPackets {
     }
 
     static List<Response> processRequests(List<Request> requests, Buffer buffer) {
-        List<Response> responses = new ArrayList<>();
+        List<Response> responses = new LinkedList<>();
         for (Request request : requests) {
             responses.add(buffer.process(request));
         }
@@ -62,7 +125,7 @@ class ProcessPackets {
     private static void printResponses(List<Response> responses) {
         for (Response response : responses) {
             if (response.dropped) {
-                System.out.println(-1);
+                System.out.println(Buffer.PACKET_WAS_DROPPED_START_TIME);
             } else {
                 System.out.println(response.startTime);
             }
--- src/test/java/io/github/vcvitaly/algo/ds/_01_basics/PacketProcessorTest.java
+++ src/test/java/io/github/vcvitaly/algo/ds/_01_basics/PacketProcessorTest.java
@@ -3,10 +3,14 @@ package io.github.vcvitaly.algo.ds._01_basics;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.rnorth.ducttape.timeouts.Timeouts;
@@ -31,6 +35,33 @@ class ProcessPacketsTest {
         ).containsExactlyElementsOf(param.responses);
     }
 
+    @Test
+    void performanceTest() {
+        int maxBufferSize = 100_000;
+        int nPackets = 100_000;
+        int maxPacketProcessingTime = 1_000;
+        int maxIntervalBetweenPackets = 1_000;
+
+        Random r = new Random(123_456_789);
+
+        Buffer buffer = new Buffer(r.nextInt(maxBufferSize));
+
+        List<Request> requests = Stream.iterate(
+                new Request(0, 1),
+                req -> new Request(
+                        req.arrivalTime + (r.nextBoolean() ? 0 : r.nextInt(maxIntervalBetweenPackets)),
+                        r.nextInt(maxPacketProcessingTime)
+                )
+        ).limit(nPackets)
+                .collect(Collectors.toList());
+
+        List<Response> responses = ProcessPackets.processRequests(requests, buffer);
+
+        Map<Boolean, Long> countByStatus = responses.stream().collect(Collectors.groupingBy(response -> response.dropped,
+                Collectors.counting()));
+        System.out.println(countByStatus);
+    }
+
     static Stream<Param> params() {
         return Stream.of(
                 Param.of(
@@ -45,13 +76,25 @@ class ProcessPacketsTest {
                 ),
                 Param.of(
                         new Buffer(1),
-                        Arrays.asList(new Request(0, 1), new Request(0, 1)),
-                        Arrays.asList(new Response(false, 0), new Response(true, -1))
+                        Arrays.asList(
+                                new Request(0, 1),
+                                new Request(0, 1)
+                        ),
+                        Arrays.asList(
+                                new Response(false, 0),
+                                new Response(true, -1)
+                        )
                 ),
                 Param.of(
                         new Buffer(1),
-                        Arrays.asList(new Request(0, 1), new Request(1, 1)),
-                        Arrays.asList(new Response(false, 0), new Response(false, 1))
+                        Arrays.asList(
+                                new Request(0, 1),
+                                new Request(1, 1)
+                        ),
+                        Arrays.asList(
+                                new Response(false, 0),
+                                new Response(false, 1)
+                        )
                 )
         );
     }
