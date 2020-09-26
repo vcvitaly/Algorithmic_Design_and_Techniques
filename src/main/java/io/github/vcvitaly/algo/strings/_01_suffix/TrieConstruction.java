d4647ebd22dd5744d9372eb4b21b30afe3e2b97d
Vitaly.Chura <vcvitaly@yahoo.com>
2020-09-26 17:25:14 +0300

    Draft TrieConstruction

---
+++ src/main/java/io/github/vcvitaly/algo/strings/_01_suffix/TrieConstruction.java
@@ -0,0 +1,68 @@
+package io.github.vcvitaly.algo.strings._01_suffix;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class TrieConstruction {
+    private static final int ROOT_NODE = 0;
+
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    List<Map<Character, Integer>> buildTrie(String[] patterns) {
+        List<Map<Character, Integer>> trie = new ArrayList<>();
+
+        for (String s : patterns) {
+            String[] pattern = s.split("");
+            int currentNode = ROOT_NODE;
+
+        }
+
+        return trie;
+    }
+
+    static public void main(String[] args) throws IOException {
+        new TrieConstruction().run();
+    }
+
+    public void print(List<Map<Character, Integer>> trie) {
+        for (int i = 0; i < trie.size(); ++i) {
+            Map<Character, Integer> node = trie.get(i);
+            for (Map.Entry<Character, Integer> entry : node.entrySet()) {
+                System.out.println(i + "->" + entry.getValue() + ":" + entry.getKey());
+            }
+        }
+    }
+
+    public void run() throws IOException {
+        FastScanner scanner = new FastScanner();
+        int patternsCount = scanner.nextInt();
+        String[] patterns = new String[patternsCount];
+        for (int i = 0; i < patternsCount; ++i) {
+            patterns[i] = scanner.next();
+        }
+        List<Map<Character, Integer>> trie = buildTrie(patterns);
+        print(trie);
+    }
+}
