5105fe80daac9552a71dab7f261b8133559a5626
Vitaly.Chura <vcvitaly@yahoo.com>
2021-01-26 20:03:53 +0200

    Draft ConnectedComponents

---
+++ src/main/java/io/github/vcvitaly/algo/graphs/_01_decomposition1/ConnectedComponents.java
@@ -0,0 +1,31 @@
+package io.github.vcvitaly.algo.graphs._01_decomposition1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConnectedComponents {
+    static int numberOfComponents(List<Integer>[] adj) {
+        int result = 0;
+        //write your code here
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            adj[x - 1].add(y - 1);
+            adj[y - 1].add(x - 1);
+        }
+        System.out.println(numberOfComponents(adj));
+    }
+}
---
+++ src/test/java/io/github/vcvitaly/algo/graphs/_01_decomposition1/ConnectedComponentsTest.java
@@ -0,0 +1,75 @@
+package io.github.vcvitaly.algo.graphs._01_decomposition1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConnectedComponentsTest {
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void findsConnectedComponents(Param param) {
+        System.out.println(param);
+
+        assertThat(
+                ConnectedComponents.numberOfComponents(
+                        toAdj(param.countOfVerticesN, param.edges)
+                )
+        ).isEqualTo(param.countOfConnectedComponents);
+    }
+
+    static Stream<Param> params() {
+        return Stream.of(
+                Param.of(
+                        4,
+                        new int[][] {
+                                new int[] {1, 2},
+                                new int[] {3, 2},
+                                new int[] {4, 3},
+                                new int[] {1, 4}
+                        },
+                        1
+                ),
+                Param.of(
+                        4,
+                        new int[][] {
+                                new int[] {1, 2},
+                                new int[] {3, 2}
+                        },
+                        2
+                )
+        );
+    }
+
+    static List<Integer>[] toAdj(int n, int[][] edges) {
+        List<Integer>[] adj = (List<Integer>[]) new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            int x, y;
+            x = edges[i][0];
+            y = edges[i][1];
+
+            adj[x - 1].add(y - 1);
+            adj[y - 1].add(x - 1);
+        }
+
+        return adj;
+    }
+
+    @Data
+    @AllArgsConstructor(staticName = "of")
+    private static class Param {
+        int countOfVerticesN;
+        private int[][] edges;
+        private int countOfConnectedComponents;
+    }
+}
