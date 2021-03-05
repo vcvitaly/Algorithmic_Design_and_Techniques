44e8be5824090022f4eec3d1367c48fcfacb2956
Vitalii Chura <c-vitalii.chura@hulu.com>
2021-03-05 16:30:07 +0200

    Draft BFS

---
+++ src/main/java/io/github/vcvitaly/algo/graphs/_03_bfs/BFS.java
@@ -0,0 +1,34 @@
+package io.github.vcvitaly.algo.graphs._03_bfs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BFS {
+
+    static final int NONE = -1;
+
+    static int distance(ArrayList<Integer>[] adj, int s, int t) {
+        //write your code here
+        return NONE;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            adj[x - 1].add(y - 1);
+            adj[y - 1].add(x - 1);
+        }
+        int x = scanner.nextInt() - 1;
+        int y = scanner.nextInt() - 1;
+        System.out.println(distance(adj, x, y));
+    }
+}
---
+++ src/test/java/io/github/vcvitaly/algo/graphs/_03_bfs/BFSTest.java
@@ -0,0 +1,61 @@
+package io.github.vcvitaly.algo.graphs._03_bfs;
+
+import io.github.vcvitaly.algo.graphs.GraphTransformationHelper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BFSTest {
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void findsMinDistanceBetweenUAndV(Param param) {
+        System.out.println(param);
+
+        assertThat(
+                BFS.distance(
+                        GraphTransformationHelper.undirectedEdgesToAdj(param.countOfVerticesN, param.edges), param.u, param.v
+                )
+        ).isEqualTo(param.minDistance);
+    }
+
+    static Stream<Param> params() {
+        return Stream.of(
+                Param.of(
+                        4,
+                        new int[][] {
+                                new int[] {1, 2},
+                                new int[] {4, 1},
+                                new int[] {2, 3},
+                                new int[] {3, 1}
+                        },
+                        2, 4, 2
+                ),
+                Param.of(
+                        5,
+                        new int[][] {
+                                new int[] {5, 2},
+                                new int[] {1, 3},
+                                new int[] {3, 4},
+                                new int[] {1, 4}
+                        },
+                        3, 5, BFS.NONE
+                )
+        );
+    }
+
+    @Data
+    @AllArgsConstructor(staticName = "of")
+    private static class Param {
+        int countOfVerticesN;
+        private int[][] edges;
+        private int u;
+        private int v;
+        private int minDistance;
+    }
+}
