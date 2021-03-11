8d825d38ac9879cf9f6a3b66cf8be15ba3f32667
Vitalii Chura <c-vitalii.chura@hulu.com>
2021-03-11 15:58:00 +0200

    Refactor Vertex and Edge

--- src/main/java/io/github/vcvitaly/algo/graphs/_03_bfs/common/Edge.java
+++ src/main/java/io/github/vcvitaly/algo/graphs/_03_bfs/common/Edge.java
@@ -12,19 +12,6 @@ public class Edge {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Edge edge = (Edge) o;
-        return v == edge.v && weight == edge.weight;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(v, weight);
-    }
-
-    @Override
     public String toString() {
         return String.format("Edge{->%d, (%d)}", v, weight);
     }
